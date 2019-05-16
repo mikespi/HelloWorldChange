52e8415d47abd7b27a5c06d8d3f00ae155df31ba
mikespi <mr.afmike@gmail.com>
2019-05-16 11:39:44 -0400

    Add Program

---
+++ hello-app/src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    private static int value = 0;
+    
+	@RequestMapping("/")
+    public String index() {
+		value++;
+        return String.format("Greetings %d from Spring Boot!", value);
+    }
+
+}
